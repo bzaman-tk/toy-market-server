0e2f7225d390a1723e755f51bc0ce62be9d9050f
bzaman_tk <bzaman.tk@gmail.com>
2023-05-19 00:36:58 +0600

    delete route added

--- index.js
+++ index.js
@@ -74,6 +74,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const result = await toyCollection.deleteOne({ _id: new ObjectId(id) })
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
